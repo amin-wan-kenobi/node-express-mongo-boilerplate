9f44437c53b97d6235901754331070cad5e81586
amin <amin@Puneet-MBP.localdomain>
2019-04-01 17:18:13 -0400

    Updated the libraries

--- server/server.js
+++ server/server.js
@@ -1,23 +1,27 @@
 require('./config/config');
- 
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
-var {mongoose} = require('./db/mongoose.js');
-const {ObjectID} = require('mongodb');
- 
+var { mongoose } = require('./db/mongoose.js');
+const { ObjectID } = require('mongodb');
+
 var app = express();
 const port = process.env.PORT;
- 
+
 app.use(bodyParser.json());
 app.use(bodyParser.text());
-  
+
 app.get('/', (req, res) => {
-    res.send('Happy Coding!!!');
+    res.send({
+        status: 200,
+        message: 'Happy Coding',
+        passedQueries: req.query
+    });
 });
- 
- 
+
+
 app.listen(port, () => console.log(`Server started and listening to Port ${port}`));
- 
- 
-module.exports = {app};
+
+
+module.exports = { app };
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -3,13 +3,34 @@ const expect = require('expect');
 const { app } = require('../server');
 
 describe('GET root path => /', () => {
-    it('should return Happy Coding!!!', (done) => {
+    it('should return an object!!!', (done) => {
         request(app)
             .get('/')
             .expect(200)
             .expect((res) => {
-                expect(res.text).toExist();
-                expect(res.text).toBe('Happy Coding!!!');
+                expect(res).toBeTruthy()
+                expect(res.body.status).toBe(200)
+                expect(res.body.message).toBe('Happy Coding')
+                expect(res.body.passedQueries).toEqual({})
+            })
+            .end((err) => {
+                if(err){
+                    return done(err);
+                }
+                done();
+            });
+    });
+    it('should receives queriy parameters', (done) => {
+        request(app)
+            .get('/?q1=queryone&q2=querytwo')
+            .expect(200)
+            .expect((res) => {
+                expect(res).toBeTruthy()
+                expect(res.body.status).toBe(200)
+                expect(res.body.message).toBe('Happy Coding')
+                expect(res.body.passedQueries).toBeTruthy()
+                expect(res.body.passedQueries.q1).toBe('queryone')
+                expect(res.body.passedQueries.q2).toBe('querytwo')
             })
             .end((err) => {
                 if(err){
